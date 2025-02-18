99f278211d7364124c716db63e70bfbabd00230e
Nic0bra <x0obubbleso0x@gmail.com>
2025-02-18 11:06:19 -0600

    update playerMovement
    did some updating

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float gravity = -1;
     Vector3 movement;
     CharacterController controller;
+    bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,13 +33,20 @@ public class PlayerMovement : MonoBehaviour
         if (controller.isGrounded)
             movement.y = 0;
 
+        //Check if grouned
+        grounded = Physics.Raycast(transform.position, Vector3.down);
+
         //Make that jawn jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             movement.y = jumpForce;
         }
 
         controller.Move(movement * Time.deltaTime);
+
+        //Change the way they are facing
+        if(xInput != 0 && zInput != 0)
+            transform.forward = new Vector3(xInput, 0, zInput);
     }
 
 //Check for collision
